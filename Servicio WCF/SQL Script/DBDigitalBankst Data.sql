07baf7258b5af28c1160e17579073976a0de8e83
Juan Diego Mejía Maestre <mejiamaestrejuandiego@gmail.com>
2023-03-17 12:36:41 -0500

    agregar insertar para usuario

---
+++ Servicio WCF/SQL Script/DBDigitalBankst Data.sql
@@ -0,0 +1,245 @@
+Use DBDigitalBankst
+go
+
+INSERT INTO Usuario(Nombre,FechaNacimiento,Sexo)
+VALUES
+  ('Cameron Hatfield','2023-04-06','F'),
+  ('Isadora Miranda','2022-04-22','M'),
+  ('Jonah Copeland','2023-11-10','F'),
+  ('Ralph Guzman','2023-05-11','F'),
+  ('Leo Jenkins','2022-08-22','F'),
+  ('Adrienne Cameron','2023-07-09','F'),
+  ('Stella Hodges','2023-10-23','M'),
+  ('Palmer Hatfield','2023-01-01','F'),
+  ('Wilma Barry','2023-02-11','F'),
+  ('Dana Clay','2024-03-01','M');
+INSERT INTO Usuario(Nombre,FechaNacimiento,Sexo)
+VALUES
+  ('Garrett Taylor','2023-05-05','M'),
+  ('Tyler Ewing','2022-04-20','F'),
+  ('Neve Workman','2023-07-21','M'),
+  ('Chava Lyons','2023-01-11','M'),
+  ('Cecilia Park','2022-09-02','M'),
+  ('Rachel Cruz','2022-09-18','M'),
+  ('Charde Livingston','2023-05-29','F'),
+  ('Sierra Hughes','2023-10-23','F'),
+  ('Hu Cleveland','2023-12-22','M'),
+  ('Conan Mcclure','2022-06-13','M');
+INSERT INTO Usuario(Nombre,FechaNacimiento,Sexo)
+VALUES
+  ('Karen Kelly','2022-06-12','F'),
+  ('Cassidy Knox','2023-08-09','M'),
+  ('Maxwell Avila','2022-09-17','F'),
+  ('Hedley Payne','2022-05-04','F'),
+  ('Amela Santos','2023-02-03','F'),
+  ('Reese Castaneda','2022-11-14','F'),
+  ('Imelda Heath','2023-09-02','F'),
+  ('Lenore Osborne','2023-09-27','M'),
+  ('Alexa Savage','2023-09-01','M'),
+  ('Porter Moses','2023-01-24','F');
+INSERT INTO Usuario(Nombre,FechaNacimiento,Sexo)
+VALUES
+  ('John Hampton','2023-12-02','M'),
+  ('Audrey Lambert','2022-04-12','F'),
+  ('Morgan Watts','2023-08-09','M'),
+  ('Darius Todd','2023-05-31','M'),
+  ('Allen Mueller','2023-04-15','M'),
+  ('Adara Trevino','2022-12-03','M'),
+  ('Murphy Alford','2022-05-18','M'),
+  ('Zachery Merrill','2023-02-02','M'),
+  ('Stuart Becker','2022-05-28','M'),
+  ('Dominic Woodard','2024-01-16','M');
+INSERT INTO Usuario(Nombre,FechaNacimiento,Sexo)
+VALUES
+  ('Chadwick Henry','2023-01-04','F'),
+  ('Angela Pratt','2022-04-26','M'),
+  ('Octavia Aguirre','2022-07-24','F'),
+  ('Vernon Bailey','2023-05-15','M'),
+  ('Eve Keith','2023-03-09','F'),
+  ('Astra Mcdaniel','2024-02-13','M'),
+  ('Hadassah Cortez','2023-03-20','M'),
+  ('Levi Rodriguez','2022-05-29','M'),
+  ('Kennan Parker','2022-09-02','F'),
+  ('Trevor Kane','2022-05-02','M');
+INSERT INTO Usuario(Nombre,FechaNacimiento,Sexo)
+VALUES
+  ('Ariel Phelps','2023-11-22','F'),
+  ('Fritz Knight','2024-01-15','F'),
+  ('Hadassah Edwards','2023-03-31','F'),
+  ('Harding Hampton','2023-05-07','M'),
+  ('Nigel Chen','2022-08-07','M'),
+  ('Vincent Cooley','2022-07-06','M'),
+  ('Catherine Wise','2022-12-02','F'),
+  ('Owen Dudley','2022-05-06','M'),
+  ('Melodie Maddox','2023-09-20','F'),
+  ('Gail Carver','2023-04-21','F');
+INSERT INTO Usuario(Nombre,FechaNacimiento,Sexo)
+VALUES
+  ('Inga Moreno','2023-10-16','F'),
+  ('Mara Mullen','2022-05-05','F'),
+  ('Uriah Flynn','2023-08-27','F'),
+  ('Logan Clemons','2022-05-09','M'),
+  ('Dacey Bradley','2022-10-01','M'),
+  ('Neve Lang','2023-07-30','F'),
+  ('Ezekiel Sharp','2022-07-29','F'),
+  ('Nadine Vinson','2023-11-29','M'),
+  ('Tiger Long','2023-06-07','F'),
+  ('Rafael Newman','2023-02-18','F');
+INSERT INTO Usuario(Nombre,FechaNacimiento,Sexo)
+VALUES
+  ('Dante Castaneda','2022-08-16','F'),
+  ('Miriam Mays','2023-04-16','F'),
+  ('Marcia Armstrong','2023-01-09','F'),
+  ('Kellie Jackson','2022-12-12','F'),
+  ('Leandra Sweeney','2022-04-20','F'),
+  ('Breanna Chase','2023-01-26','F'),
+  ('Nehru Santos','2023-09-13','F'),
+  ('Gannon Ware','2023-11-28','M'),
+  ('Yeo Jacobson','2023-12-29','F'),
+  ('Jameson Wilcox','2023-12-12','M');
+INSERT INTO Usuario(Nombre,FechaNacimiento,Sexo)
+VALUES
+  ('Mohammad King','2023-07-04','F'),
+  ('Mariam Nguyen','2022-08-11','M'),
+  ('Justina Woods','2023-07-31','F'),
+  ('Basil Cox','2023-12-21','F'),
+  ('Herman Hancock','2022-12-26','F'),
+  ('Elmo Aguilar','2022-08-21','F'),
+  ('Maxine Barron','2024-03-10','F'),
+  ('Colin Savage','2023-04-04','M'),
+  ('Cain Holt','2022-05-07','M'),
+  ('Jelani Hood','2022-05-08','M');
+INSERT INTO Usuario(Nombre,FechaNacimiento,Sexo)
+VALUES
+  ('Skyler Gross','2022-05-23','F'),
+  ('Ryan Underwood','2023-11-19','M'),
+  ('Skyler Carpenter','2022-09-14','F'),
+  ('Ivory Mccarthy','2022-08-22','M'),
+  ('Honorato Merrill','2024-01-20','M'),
+  ('Mara Rojas','2023-08-23','M'),
+  ('Nicholas Mcleod','2022-09-16','M'),
+  ('Camille Sykes','2022-05-30','F'),
+  ('Amaya Green','2023-09-15','M'),
+  ('Fuller Farmer','2022-06-28','M');
+INSERT INTO Usuario(Nombre,FechaNacimiento,Sexo)
+VALUES
+  ('Simon Barrett','2023-02-22','M'),
+  ('Yasir Rivera','2023-12-03','F'),
+  ('Devin Harrell','2023-12-29','F'),
+  ('Chiquita Carrillo','2023-07-15','M'),
+  ('Cameron May','2023-12-22','F'),
+  ('Adrian Soto','2023-02-27','M'),
+  ('Harlan Woods','2022-12-30','F'),
+  ('Jarrod Underwood','2022-05-11','M'),
+  ('Maile Hewitt','2023-07-02','F'),
+  ('Cameron Miles','2024-02-14','F');
+
+INSERT INTO Usuario(Nombre,FechaNacimiento,Sexo)
+VALUES
+  ('Harper Duncan','2022-07-02','M'),
+  ('Sophia Paul','2023-04-24','F'),
+  ('Britanney Singleton','2022-10-06','M'),
+  ('Phelan Hurst','2022-04-25','F'),
+  ('Pandora Dodson','2023-07-25','M'),
+  ('Cara O Neill','2023-11-21','F'),
+  ('Lacota Dillon','2023-01-28','F'),
+  ('Carter Dickerson','2023-03-11','M'),
+  ('Lee Potter','2022-07-20','M'),
+  ('Brooke Mcintosh','2023-04-11','F');
+
+INSERT INTO Usuario(Nombre,FechaNacimiento,Sexo)
+VALUES
+  ('Andrew Morton','2023-12-30','M'),
+  ('Jenette Padilla','2023-02-02','F'),
+  ('Ian Workman','2024-02-25','M'),
+  ('Quemby Lowe','2023-09-16','M'),
+  ('Isaac Gonzales','2023-03-27','M'),
+  ('Arden Suarez','2022-04-16','F'),
+  ('Garth Knight','2024-02-03','M'),
+  ('Chase Hill','2022-05-19','F'),
+  ('Ferris Mullins','2024-03-01','M'),
+  ('Thaddeus Roberson','2023-05-08','M');
+INSERT INTO Usuario(Nombre,FechaNacimiento,Sexo)
+VALUES
+  ('Ethan Montgomery','2023-05-16','F'),
+  ('Aphrodite Carroll','2022-11-19','F'),
+  ('Ira Cortez','2022-03-21','F'),
+  ('Zena Larsen','2022-07-08','M'),
+  ('Drake Hendricks','2022-06-30','F'),
+  ('Beverly Hays','2022-05-16','F'),
+  ('Rebecca Watson','2022-05-21','F'),
+  ('Finn Zimmerman','2023-06-07','M'),
+  ('Zeus Vinson','2023-09-03','M'),
+  ('Jin Snider','2023-05-13','F');
+INSERT INTO Usuario(Nombre,FechaNacimiento,Sexo)
+VALUES
+  ('Isaac Rush','2022-08-05','M'),
+  ('Callie Moss','2022-08-07','F'),
+  ('Stacey Hardin','2022-06-19','F'),
+  ('Tanisha Santana','2022-04-11','F'),
+  ('Keiko Baxter','2022-09-23','F'),
+  ('Keith Molina','2023-06-18','F'),
+  ('Noelle Collier','2023-10-08','M'),
+  ('Baker Joyner','2022-05-24','F'),
+  ('Dara Vaughn','2023-06-16','F'),
+  ('Cade Sweet','2024-01-19','F');
+INSERT INTO Usuario(Nombre,FechaNacimiento,Sexo)
+VALUES
+  ('Stephanie Keith','2022-12-18','M'),
+  ('Gabriel Briggs','2023-03-06','F'),
+  ('Laura Moon','2022-12-14','M'),
+  ('Dillon Winters','2023-06-21','F'),
+  ('Ebony Graham','2023-01-14','M'),
+  ('Hector Bean','2022-11-27','F'),
+  ('Jeremy Wade','2022-08-11','F'),
+  ('Addison Stanley','2022-05-29','F'),
+  ('Michelle Zimmerman','2023-04-10','F'),
+  ('Tyrone Guzman','2024-02-25','M');
+INSERT INTO Usuario(Nombre,FechaNacimiento,Sexo)
+VALUES
+  ('Otto Collins','2022-08-21','M'),
+  ('Astra Mckenzie','2023-09-23','F'),
+  ('Dane Cole','2022-12-18','F'),
+  ('Branden Harris','2024-01-29','F'),
+  ('Cheryl Brady','2022-05-03','F'),
+  ('Yvette Snow','2022-07-04','M'),
+  ('Alec Dominguez','2022-05-04','M'),
+  ('Jonas Leon','2022-07-27','M'),
+  ('Charde Camacho','2023-11-18','F'),
+  ('Isaiah Everett','2022-10-31','F');
+INSERT INTO Usuario(Nombre,FechaNacimiento,Sexo)
+VALUES
+  ('Ina Long','2023-09-13','F'),
+  ('Yolanda Atkinson','2023-11-13','F'),
+  ('Pearl Emerson','2023-01-17','M'),
+  ('Ella Sampson','2022-08-01','M'),
+  ('William Morrow','2023-12-08','M'),
+  ('Bianca Moreno','2023-01-02','M'),
+  ('Rama Faulkner','2022-08-28','M'),
+  ('Sydnee Mcfadden','2023-02-24','M'),
+  ('Remedios Daugherty','2022-04-22','F'),
+  ('Berk Richmond','2024-01-12','F');
+INSERT INTO Usuario(Nombre,FechaNacimiento,Sexo)
+VALUES
+  ('Walker Gallegos','2022-05-23','M'),
+  ('Virginia Hawkins','2023-02-11','M'),
+  ('Whitney Fernandez','2022-07-07','F'),
+  ('Madeson Webb','2022-03-20','M'),
+  ('Jena Mcguire','2023-08-18','M'),
+  ('Brandon Gallagher','2023-05-18','F'),
+  ('Quin Mooney','2022-04-04','F'),
+  ('Helen Nunez','2023-09-03','M'),
+  ('Nicholas Freeman','2022-06-06','M'),
+  ('Ethan Elliott','2023-12-25','F');
+INSERT INTO Usuario(Nombre,FechaNacimiento,Sexo)
+VALUES
+  ('Hayley Anderson','2022-12-09','F'),
+  ('Chadwick Cohen','2022-12-01','F'),
+  ('Maite Alvarez','2023-12-02','F'),
+  ('Joel Hill','2023-11-01','M'),
+  ('Kelsey Huffman','2022-07-05','F'),
+  ('Donovan Rowe','2023-11-13','M'),
+  ('Graham Foster','2022-08-20','F'),
+  ('Melissa Foster','2023-07-30','M'),
+  ('Rebekah Hawkins','2023-05-30','F'),
+  ('Drew White','2022-10-13','F');
